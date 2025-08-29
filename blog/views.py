05e4633b7d43629500cb8f53293cae72730f8d01
somayehhosseinbeig <somayeh.hosseinbeig@gmail.com>
2025-08-30 00:24:09 +0330

    create blogsingle

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from blog.models import Post
 
 # Create your views here.
 def blog_view(request):
-     return render(request,'blog/blog-home.html')
+     posts = Post.objects.filter(status=1)
+     context = {'posts':posts}
+     return render(request,'blog/blog-home.html',context)
 
-def blog_single(request):
-     context = {'title':'bitcoin crashed again!', 'content': 'bitcoin was flying but now grounded as always', 'author':'somayehhosseinbeig'}
-     return render(request,'blog/blog-single.html', context)
+def blog_single(request,pid):
+     post = get_object_or_404(Post,pk=pid)
+     context = {'post':post}
+     return render(request,'blog/blog-single.html',context)
+
+     #context = {'title':'bitcoin crashed again!', 'content': 'bitcoin was flying but now grounded as always', 'author':'somayehhosseinbeig'}
+     #return render(request,'blog/blog-single.html', context)
+     
+""" def test(request):
+     posts = Post.objects.all()
+     # posts = Post.objects.filter(status=0)
+     context = {'posts':posts}
+     return render(request,'test.html',context) """
+
+def test(request,pid):
+     #post = Post.objects.get(id=pid)
+     post = get_object_or_404(Post,pk=pid)
+     context = {'post':post}
+     return render(request,'test.html',context)
