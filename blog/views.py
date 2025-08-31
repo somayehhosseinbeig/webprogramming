96ce6db0e4073c5e9eb33909148d46a5df88a29c
somayehhosseinbeig <somayeh.hosseinbeig@gmail.com>
2025-08-31 22:10:58 +0330

    Completed Django form for contact and newsletter
    Completed Django form for contact and newsletter and also a meesage for success or error

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render,get_object_or_404
 from blog.models import Post
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 
 # Create your views here.
-def blog_view(request):
+def blog_view(request,**kwargs):
      posts = Post.objects.filter(status=1)
+     if kwargs.get('cat_name') != None:
+          posts = posts.filter(category__name=kwargs['cat_name'])
+     if kwargs.get('author_username') != None:
+          posts = posts.filter(author__username = kwargs['author_username'])
+     posts = Paginator(posts,3)
+     try:
+          page_number = request.GET.get('page')
+          posts = posts.get_page(page_number)
+     except PageNotAnInteger:
+          posts = posts.get_page(1)
+     except EmptyPage:
+          posts = posts.get_page(1)
      context = {'posts':posts}
      return render(request,'blog/blog-home.html',context)
 
@@ -37,3 +50,12 @@ def blog_category(request,cat_name):
      context = {'posts':posts}
      return render(request,'blog/blog-home.html',context)
 
+def blog_search(request):
+     posts = Post.objects.filter(status=1)
+     if request.method == 'GET' :
+          #print(request.GET.get('s'))
+          if s :=request.GET.get('s'):
+               posts = posts.filter(content__contains=s)
+     
+     context = {'posts':posts}
+     return render(request,'blog/blog-home.html',context)
--- website/views.py
+++ website/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from website.models import Contact
+from website.forms import ContactForm, NewsletterForm
+from django.contrib import messages
 
 def index_view(request):
     return render(request,'website/index.html')
@@ -9,8 +12,31 @@ def about_view(request):
 
 
 def contact_view(request):
-    return render(request,'website/contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request,messages.SUCCESS,'your ticket submited successfully')
+        else:
+            messages.add_message(request,messages.ERROR,'your ticket didnt submited')
+    form = ContactForm()
+    return render(request,'website/contact.html',{'form':form})
+
+""" def test_view(request):
+
+    return render(request,'website/test.html',{'name':'Somayeh', 'lastname':'Hosseinbeig'}) """
+
+def newsletter_view(request):
+    if request.method == 'POST':
+        form = NewsletterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    else:
+        return HttpResponseRedirect('/')
+
 
 def test_view(request):
-
-    return render(request,'website/test.html',{'name':'Somayeh', 'lastname':'Hosseinbeig'})
+    if request.method == 'POST':
+       print(request.POST.get('name'))
+    return render(request, 'test.html',{})
